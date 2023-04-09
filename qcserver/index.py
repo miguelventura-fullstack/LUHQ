77591da52937e15b55821e42b9a6708e8023df2c
Jaden Hou <jadenhou19@gmail.com>
2023-04-08 22:11:29 -0400

    cors

--- qcserver/index.py
+++ qcserver/index.py
@@ -14,10 +14,23 @@ from qiskit_finance.data_providers import *
 from qiskit_finance import QiskitFinanceError
 import numpy as np
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def quant(stocks, startDatetime, endDatetime, budget, risk):
